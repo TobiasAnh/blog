10e0ed1fc895a033807472325008e564675fe2cf
tanh <tanhaeuser@gmail.com>
2022-06-06 16:58:35 +0200

    initial commit2

--- blog/tests.py
+++ blog/tests.py
@@ -34,5 +34,15 @@ class BlogTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_post_listview(self):
-        reponse = self.client.get(reverse("home"))
-        self
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Nice body content")
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        no_response = self.client.get("/post/100000/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "A good title")
+        self.assertTemplateUsed(response, "post_detail.html")
