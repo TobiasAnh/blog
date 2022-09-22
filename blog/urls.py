efc514718d0e84073aa4375e4f48a88965f701e6
turbo <tanhaeuser@gmail.com>
2022-09-22 11:55:43 +0200

    revisiting django after some time

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from blog.views import (
-    BlogListView, 
-    BlogDetailView, 
+    BlogListView,
+    BlogDetailView,
     BlogCreateView,
     BlogUpdateView,
     BlogDeleteView,
@@ -10,7 +10,7 @@ from blog.views import (
 urlpatterns = [
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
-    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name = "post_edit"),
-    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name = "post_delete"),
+    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name="post_delete"),
     path("", BlogListView.as_view(), name="home"),
 ]
